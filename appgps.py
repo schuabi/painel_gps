c8700bd2697ebd5af1d3644531b0403db8e5c81c
schuabi <schuabi@gmail.com>
2025-06-21 12:44:04 -0300

    atualiza├º├úo dados gps

--- appgps.py
+++ appgps.py
@@ -16,6 +16,15 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+# ✅ Configurar Auto-Refresh a cada 5 minutos
+def auto_refresh(interval=300):
+    def refresh():
+        time.sleep(interval)
+        st.experimental_rerun()
+    threading.Thread(target=refresh, daemon=True).start()
+
+auto_refresh()
+
 def app():
     # ✅ Leitura dos dados
     gps_nucleo = pd.read_csv("dados/dados_gps.csv", sep=";")
