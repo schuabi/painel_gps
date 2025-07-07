7bbb5cba064071772de3545a9fa17fe2289b8fe5
schuabi <schuabi@gmail.com>
2025-07-07 06:20:14 -0300

    atualiza├º├úo dados gps

--- appgps.py
+++ appgps.py
@@ -124,14 +124,7 @@ def app():
     </div>
 """, unsafe_allow_html=True)
 
-# ✅ Configurar Auto-Refresh a cada 5 minutos
-def auto_refresh(interval=300):
-    def refresh():
-        time.sleep(interval)
-        st.experimental_rerun()
-    threading.Thread(target=refresh, daemon=True).start()
 
-auto_refresh()
 
     
 
